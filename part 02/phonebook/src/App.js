7e53f3d07126d90c656a8ccafab1872c2e3d2e53
Alexandru Radu-Todor <50906589+AlexandruRT631@users.noreply.github.com>
2021-07-28 00:01:04 +0300

    solved 2.6 - 2.10

---
+++ part 02/phonebook/src/App.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas', number: '040-123456'},
+        {name: 'Ada Lovelace', number: '39-44-5323523'},
+        {name: 'Dan Abramov', number: '12-43-234345'},
+        {name: 'Mary Poppendieck', number: '39-23-6423122'}
+    ])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [newSearch, setNewSearch] = useState('')
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        const newPerson = {
+            name: newName,
+            number: newNumber
+        }
+
+        const indexPerson = persons.map(persons => persons.name.toLowerCase()).indexOf(newPerson.name.toLowerCase())
+
+        if (indexPerson === -1) {
+            setPersons(persons.concat(newPerson))
+        } else {
+            window.alert(`${newName} is already added to phonebook`)
+        }
+        setNewName('')
+        setNewNumber('')
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+    const handleSearchChange = (event) => {
+        setNewSearch(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter newSearch={newSearch} handleSearchChange={handleSearchChange}/>
+            <h2>Add a new</h2>
+            <PersonForm newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} addPerson={addPerson}/>
+            <h2>Numbers</h2>
+            <Persons persons={persons} newSearch={newSearch}/>
+        </div>
+    )
+}
+
+export default App
---
+++ part 02/phonebook/src/components/Persons.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Person = ({person}) => {
+    return (
+        <li>{person.name} {person.number}</li>
+    )
+}
+const Persons = (props) => {
+    return (
+        <ul>
+            {
+                props.persons.filter(person => person.name.toLowerCase().includes(props.newSearch.toLowerCase()) === true)
+                    .map(person => <Person key={person.name} person={person}/>)
+            }
+        </ul>
+    )
+}
+
+export default Persons
