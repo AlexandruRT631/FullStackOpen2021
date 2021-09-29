5173b0fec060fa22071058bcdfa37250af16a2d4
Alexandru Radu-Todor <50906589+AlexandruRT631@users.noreply.github.com>
2021-09-29 21:10:16 +0300

    soved 3.19 - 3.21

--- part 03/phonebook backend/models/person.js
+++ part 03/phonebook backend/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -14,8 +15,17 @@ mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
     id: Number,
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minlength: 3,
+        unique: true,
+        required: true
+    },
+    number: {
+        type: String,
+        minlength: 8,
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
@@ -25,5 +35,7 @@ personSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
+
+personSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', personSchema)
